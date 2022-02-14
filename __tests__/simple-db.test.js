aeb9da901b69c03806cb360c9661bcb5ad9cb0b3
Frank Hollywood <github@srvsync.com>
2022-02-14 14:43:44 -0800

    get by id and test

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,19 +1,25 @@
 const fs = require('fs/promises');
 const path = require('path');
+const SimpleDB = require('../lib/simple-DB.js');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
 const TEST_DIR = path.join(BASE_DIR, 'test-dir');
 
 describe('simple database', () => {
-
   beforeEach(async () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
-
+  it('gets by id', async () => {
+    const firstDb = new SimpleDB(TEST_DIR);
+    const newFile = {
+      id: '1',
+      content: 'new file',
+    };
+    const filePath = path.join(TEST_DIR, `${newFile.id}.txt`);
+    await fs.writeFile(filePath, JSON.stringify(newFile));
+    expect(await firstDb.get(newFile.id)).toEqual(newFile);
   });
-
 });
